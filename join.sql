c7acd3908043ef5b080ca96612ff4a57b545e01f
Marcos Paulo Almeida <41038049+Marcos14Almeida@users.noreply.github.com>
2022-04-11 21:25:57 -0300

    Update join.sql

--- join.sql
+++ join.sql
@@ -15,6 +15,12 @@ ON table1.column_name = table2.column_name;
 SELECT Orders.OrderID, Customers.CustomerName, Orders.OrderDate
 FROM Orders
 INNER JOIN Customers ON Orders.CustomerID=Customers.CustomerID;
+
+-- 3 TABELAS
+SELECT Orders.OrderID, Customers.CustomerName, Shippers.ShipperName
+FROM ((Orders
+INNER JOIN Customers ON Orders.CustomerID = Customers.CustomerID)
+INNER JOIN Shippers ON Orders.ShipperID = Shippers.ShipperID);
 --------------------------------------------------------------
                         LEFT JOIN
 --------------------------------------------------------------
