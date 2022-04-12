c4631a1856a24642c4e8c930e060b39dbd874e12
Marcos Paulo Almeida <41038049+Marcos14Almeida@users.noreply.github.com>
2022-04-11 21:03:46 -0300

    Create between.sql

---
+++ between.sql
@@ -0,0 +1,7 @@
+SELECT * FROM Products
+WHERE Price BETWEEN 10 AND 20
+AND CategoryID NOT IN (1,2,3);
+
+SELECT * FROM Products
+WHERE ProductName BETWEEN 'Carnarvon Tigers' AND 'Mozzarella di Giovanni'
+ORDER BY ProductName;
