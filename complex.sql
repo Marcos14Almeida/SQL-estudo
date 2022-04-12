45a7c8293f725ba5d2571277532909667acfb3f7
Marcos Paulo Almeida <41038049+Marcos14Almeida@users.noreply.github.com>
2022-04-11 23:04:08 -0300

    Update complex.sql

--- complex.sql
+++ complex.sql
@@ -4,8 +4,7 @@
 -- Agrupa pelas diferentes metas e resultados
 -- Tudo isso sem considerar o ano de 1997
 SELECT OrderDate, MONTH(OrderDate)*2 As Meta, DAY(OrderDate) AS Dia, MONTH(OrderDate) AS Mes, YEAR(OrderDate) AS Ano, 
-IF(DAY(OrderDate)>MONTH(OrderDate)*2,'Sucesso',0) AS Resul, COUNT(IF(DAY(OrderDate)>MONTH(OrderDate)*2,'Sucesso',0))
-FROM Orders 
+IF(DAY(OrderDate)>MONTH(OrderDate)*2,'Sucesso',0) AS Resul, COUNT(IF(DAY(OrderDate)>MONTH(OrderDate)*2,'Sucesso',0)) AS total
 WHERE YEAR(OrderDate) <> 1997
 GROUP BY Meta,Ano;
 
