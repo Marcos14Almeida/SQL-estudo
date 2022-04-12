9a5f0ba06c211455078716ac7938e7a8b3ffd665
Marcos Paulo Almeida <41038049+Marcos14Almeida@users.noreply.github.com>
2022-04-11 21:24:45 -0300

    Update join.sql

--- join.sql
+++ join.sql
@@ -4,6 +4,25 @@ RIGHT JOIN: Returns all records from the right table, and the matched records fr
 CROSS JOIN: Returns all records from both tables
 
 --------------------------------------------------------------
+                        INNER JOIN
+--------------------------------------------------------------
+SELECT column_name(s)
+FROM table1
+INNER JOIN table2
+ON table1.column_name = table2.column_name;
+
+
 SELECT Orders.OrderID, Customers.CustomerName, Orders.OrderDate
 FROM Orders
 INNER JOIN Customers ON Orders.CustomerID=Customers.CustomerID;
+--------------------------------------------------------------
+                        LEFT JOIN
+--------------------------------------------------------------
+
+--------------------------------------------------------------
+                        RIGHT JOIN
+--------------------------------------------------------------
+
+--------------------------------------------------------------
+                        CROSS JOIN
+--------------------------------------------------------------
