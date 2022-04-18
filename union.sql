007463171f66d05a46d6a3013d7ab99d736646eb
Marcos Paulo Almeida <41038049+Marcos14Almeida@users.noreply.github.com>
2022-04-18 16:27:51 -0300

    Update union.sql

--- union.sql
+++ union.sql
@@ -3,7 +3,9 @@ The UNION operator is used to combine the result-set of two or more SELECT state
 Every SELECT statement within UNION must have the same number of columns
 The columns must also have similar data types
 The columns in every SELECT statement must also be in the same order
-The UNION operator selects only distinct values by default. To allow duplicate values, use UNION ALL:
+The UNION operator selects only distinct values by default. 
+
+To allow duplicate values, use UNION ALL:
 --------------------------------------------
 
 SELECT City,Address FROM Customers
